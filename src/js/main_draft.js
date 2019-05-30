e0046f38b7519c4f79e253792de72d6f7c3831b6
hEy <hey259@gmail.com>
2019-05-30 13:03:19 -0500

    Add template code
    Add main comment block including project name, version description and toc
    Add keywords to be replaced with brunch keyword plugin
    Add basic comments and code for a starter template

---
+++ src/js/main_draft.js
@@ -0,0 +1,25 @@
+/*! {!project_name!} v0.1.0 */
+/**
+ * JS for: {!project_name!}
+ *
+ * Author: {!dev_alias!} @ {!dev_agency!}
+ *
+ * TOC:
+ *
+ * Modules
+ * jQuery document ready
+ * 
+ * @todo Add each module/section needed
+ * 
+ */
+/* Modules */
+const $ = require('jquery');
+
+
+/**
+ * jQuery document ready
+ */
+$(document).ready(function()
+{
+
+});
